fcb4d587a886a8241ab199f17b75c4c8c26a1ed4
arccosine <40669+ArcCosine@users.noreply.github.com>
2021-04-12 01:14:38 +0900

    [Add] First commit .

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import styles from './index.module.css'
+
+
+const Index:NextPage = () => {
+    return <h1 className={styles.heading}>Kindness</h1>
+}
+
+export default Index
