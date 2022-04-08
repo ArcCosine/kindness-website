8b2d15a71a33537ebb28a0d81bba44792cd5cfde
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-04-09 03:19:57 +0900

    [delete] all refresh.

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app'
-
-import 'tailwindcss/tailwind.css';
-import 'tailwindcss/components.css';
-import 'tailwindcss/utilities.css';
-
-export default function App({ Component, pageProps }: AppProps ) {
-    return <Component {...pageProps} />
-}
