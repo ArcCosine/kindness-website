3cb92e7c1aea3c61d23a33729cf58317ad63cce1
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-05-08 12:30:27 +0900

    [Update] Add Icons.
    Move manifest.webmanifest.

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: NextPage = ()=>{
             />
             <link rel="manifest" href="/manifest.webmanifest" />
             <link rel="icon" href="/favicon.png" />
-            <link rel="apple-touch-icon" href="/icon-192x192.png" />
+            <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
         </Head>
     )
 }
